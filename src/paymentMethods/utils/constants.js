79e3aeb8edb7c88de9c549b860d54476e17b2f29
Latitude Checkout <dheeraj.khushalani@latitudefinancial.com>
2021-03-23 13:44:20 +1100

    fix: typo

--- src/paymentMethods/utils/constants.js
+++ src/paymentMethods/utils/constants.js
@@ -5,7 +5,7 @@ export const LOGO_SRC =
   "https://assets.latitudefinancial.com/merchant-services/latitude/icon/latitude-interest-free.svg";
 
 export const CONTENT_SRC =
-  "https://checkout.latitudefinancial.com/assets/content.js?platform=magento2";
+  "https://checkout.latitudefinancial.com/assets/content.js?platform=direct";
 
 export const PAYMENT_CONTAINER_MAIN = "latitude-payment--main";
 export const PAYMENT_CONTAINER_FOOTER = "latitude-payment--footer";
